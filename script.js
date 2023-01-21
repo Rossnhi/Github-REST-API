1a17c2350a1c93894c71b70da293492da5d9cd6d
Roshini <roshni.dabriya@gmail.com>
2023-01-21 23:24:18 +0530

    created frontend

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function dumpResponse() {
-  // `this` will refer to the `XMLHTTPRequest` object that executes this function
-  console.log(this.responseText);
-}
-var request = new XMLHttpRequest();
-// Set the event handler
-request.onload = dumpResponse;
-// Initialize the request
-request.open('get', 'https://api.github.com/users/rossnhi', true);
-// Fire away!
-request.send();
+let data;
+let nameList = document.getElementById('bookNameList');
+let authorList = document.getElementById('bookAuthorList');
 
-const data = `<html> <head> </head> <body> <h1> HEY THIS IS WORKING!</h1></body></html>`;              //sample json
-const a = document.createElement('a');
-const blob = new Blob([data], {type: "text/html"});
-a.href = URL.createObjectURL(blob);
-a.download = 'test';                     //filename to download
-a.click();
+getData() 
+
+function getData() {
+  let request = new XMLHttpRequest();
+  // Set the event handler
+  request.onload = dumpResponse;
+  // Initialize the request
+  request.open('get', 'https://api.github.com/repos/Rossnhi/Github-REST-API/contents/data.json', true);
+  // Fire away!
+  request.send();
+}
+
+function dumpResponse() {
+  let resp = JSON.parse(this.responseText);
+  data = JSON.parse(atob(resp.content)); // resp.content will be base64 encoded atob() decodes it
+
+  insertIntoBookList();
+
+}
+
+function insertIntoBookList() {
+	for (let entry of data.books) {
+		let name = document.createElement('li');
+        name.textContent = entry.name;
+
+        let author = document.createElement('li');
+        author.textContent = entry.author;
+
+        nameList.appendChild(name);
+        authorList.appendChild(author);
+	}
+}
